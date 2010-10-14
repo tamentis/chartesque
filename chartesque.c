615e60f3abb1a118f8620edd890425aae7ae0a53
Bertrand Janin <tamentis@neopulsar.org>
2010-10-14 19:01:34 -0400

    Added drawing of the x-axis

--- chartesque.c
+++ chartesque.c
@@ -65,12 +65,13 @@ typedef struct _dataplot_chart_t {
 } dataplot_chart_t;
 
 /**
- * Given an exact font definition and an UTF-8 string, return the estimated
- * width of the piece of text.
+ * Given an exact font definition and an UTF-8 string, set the width and
+ * height pointers, it does not return anything.
  */
-double
-get_text_width(cairo_t *cr, const char *family, cairo_font_slant_t slant,
-		cairo_font_weight_t weight, double size, const char *utf8)
+void
+get_text_size(cairo_t *cr, const char *family, cairo_font_slant_t slant,
+		cairo_font_weight_t weight, double size, const char *utf8,
+		double *width, double *height)
 {
 	cairo_text_extents_t extents;
 
@@ -80,26 +81,8 @@ get_text_width(cairo_t *cr, const char *family, cairo_font_slant_t slant,
 	cairo_text_extents(cr, utf8, &extents);
 	cairo_restore(cr);
 
-	return extents.width;
-}
-
-/**
- * Given an exact font definition and an UTF-8 string, return the estimated
- * height of the piece of text.
- */
-double
-get_text_height(cairo_t *cr, const char *family, cairo_font_slant_t slant,
-		cairo_font_weight_t weight, double size, const char *utf8)
-{
-	cairo_text_extents_t extents;
-
-	cairo_save(cr);
-	cairo_select_font_face(cr, family, slant, weight);
-	cairo_set_font_size(cr, size);
-	cairo_text_extents(cr, utf8, &extents);
-	cairo_restore(cr);
-
-	return extents.height;
+	*width = extents.width;
+	*height = extents.height;
 }
 
 /**
@@ -110,8 +93,16 @@ dataplot_chart_set_y_limit(dataplot_chart_t *chart, double min, double max)
 {
 	chart->y_limit_min = min;
 	chart->y_limit_max = max;
+}
 
-
+/**
+ * Set the limit, boundaries of the x axis.
+ */
+void
+dataplot_chart_set_x_limit(dataplot_chart_t *chart, double min, double max)
+{
+	chart->x_limit_min = min;
+	chart->x_limit_max = max;
 }
 
 /**
@@ -144,6 +135,7 @@ dataplot_chart_new()
 
 	chart->x_label_fontfamily = strdup("Sans");
 	chart->x_label_fontsize = 10.0;
+	chart->x_label_padding = 4.0;
 
 	chart->y_label_fontfamily = strdup("Sans");
 	chart->y_label_fontsize = 10.0;
@@ -177,13 +169,47 @@ dataplot_chart_render_y_label_text(dataplot_chart_t *chart, double y, char *text
 
 	cairo_text_extents(chart->cr, text, &extents);
 
-	printf("y=%f\n", chart->margin_top + y);
-
 	cairo_set_font_size(chart->cr, chart->y_label_fontsize);
 	cairo_move_to(chart->cr, chart->margin_left + chart->y_label_padding +
 			chart->y_label_max_width - extents.width,
 			chart->margin_top + chart->y_label_padding + y);
 	cairo_text_path(chart->cr, text);
+}
+
+/**
+ * Return the physical y position of the text labels.
+ */
+double
+dataplot_chart_get_x_label_y(dataplot_chart_t *chart)
+{
+	return chart->height - chart->margin_bottom - chart->x_label_padding;
+}
+
+/**
+ * Render a label for the x-axis.
+ */
+void
+dataplot_chart_render_x_label_text(dataplot_chart_t *chart, double x, char *text)
+{
+	cairo_text_extents_t extents;
+	double x_label_y;
+
+	cairo_text_extents(chart->cr, text, &extents);
+	x_label_y = dataplot_chart_get_x_label_y(chart);
+
+	cairo_set_font_size(chart->cr, chart->x_label_fontsize);
+	cairo_move_to(chart->cr, chart->margin_left +
+			chart->y_label_padding * 2.0 +
+			chart->y_label_max_width + x -
+			extents.width / 2.0, x_label_y);
+	cairo_text_path(chart->cr, text);
+}
+
+double
+dataplot_chart_get_y_axis_height(dataplot_chart_t *chart)
+{
+	return chart->height - chart->margin_top - chart->margin_bottom -
+		chart->x_label_padding * 2 - chart->x_label_max_height;
 }
 
 /**
@@ -194,13 +220,28 @@ dataplot_chart_render_y_label_text(dataplot_chart_t *chart, double y, char *text
 double
 dataplot_chart_convert_to_y_scale(dataplot_chart_t *chart, double value)
 {
-	double y_axis_height = chart->height - chart->margin_top -
-		chart->margin_bottom;
+	double y_axis_height = dataplot_chart_get_y_axis_height(chart);
 	double y_limit_spread = chart->y_limit_max - chart->y_limit_min;
-
 	double value_ratio = (value - chart->y_limit_min) / y_limit_spread;
 
 	return y_axis_height - y_axis_height * value_ratio;
+}
+
+/**
+ * Convert an actual data value to a x coordinate for drawing. This excludes
+ * all margin and padding calculations, it outputs the coordinates from the
+ * axis itself.
+ */
+double
+dataplot_chart_convert_to_x_scale(dataplot_chart_t *chart, double value)
+{
+	double x_axis_width = chart->width - chart->margin_left -
+		chart->margin_right;
+	double x_limit_spread = chart->x_limit_max - chart->x_limit_min;
+
+	double value_ratio = (value - chart->x_limit_min) / x_limit_spread;
+
+	return x_axis_width * value_ratio;
 }
 
 /**
@@ -230,20 +271,70 @@ dataplot_chart_render_y_axis_labels(dataplot_chart_t *chart)
 }
 
 void
+dataplot_chart_render_x_axis_labels(dataplot_chart_t *chart)
+{
+	unsigned int i;
+
+	cairo_select_font_face (chart->cr, chart->x_label_fontfamily,
+			   CAIRO_FONT_SLANT_NORMAL,
+			   CAIRO_FONT_WEIGHT_BOLD);
+
+	for (i = 0; i < chart->x_ticks_count; i++) {
+		dataplot_chart_render_x_label_text(chart,
+				chart->x_ticks_positions[i],
+				chart->x_ticks_labels[i]);
+	}
+}
+
+/**
+ * Determine the width/height of a double value rendered to text using the
+ * provided parameters. The values are set directly on the *width and *height
+ * pointers, the rendered text value is returned and needs to be free'd. Make
+ * sure you pass copy = 0 if you don't need the output or you'll be creating
+ * a memory leak.
+ *
+ * I have the feeling this function should really be two, but let's see how
+ * much we can fuck up the whole library without refactoring.
+ */
+char *
+dataplot_chart_prerender_value(dataplot_chart_t *chart, double value, 
+		const char *family, cairo_font_slant_t slant,
+		cairo_font_weight_t weight, double size, double *width,
+		double *height, int copy)
+{
+	char lbuffer[MAX_LABEL_SIZE];
+
+	snprintf(lbuffer, MAX_LABEL_SIZE, "%.1f", value);
+
+	if (width != NULL && height != NULL) {
+		get_text_size(chart->cr, family, slant, weight,
+				size, lbuffer, width, height);
+	}
+
+	if (copy) {
+		return strdup(lbuffer);
+	} else {
+		return NULL;
+	}
+}
+
+void
 dataplot_chart_render_y_axis_ticks(dataplot_chart_t *chart)
 {
 	unsigned int i;
-	size_t len;
-	double width, height, max_label_width = 0.0, max_label_height = 0.0;
-	char lbuffer[MAX_LABEL_SIZE];
+	double value;
+	char *rendered;
 
 	/* This is the size of the y-axis line */
-	double y_axis_height = chart->height - chart->margin_top -
-		chart->margin_bottom;
+	double y_axis_height = dataplot_chart_get_y_axis_height(chart);
 
 	double y_limit_spread = chart->y_limit_max - chart->y_limit_min;
 
-	/* This is the number of labels we would like to have shown */
+	/* This is the number of labels we would like to have shown
+	 *
+	 * This is not clear, max_height is always 0 at this point, rephrase
+	 * to include the fontsize maybe?
+	 */
 	chart->y_ticks_count = (y_axis_height / (chart->y_label_padding * 2.0 +
 				chart->y_label_max_height)) / 2;
 
@@ -254,28 +345,120 @@ dataplot_chart_render_y_axis_ticks(dataplot_chart_t *chart)
 	double y_tick_value_spacing = y_limit_spread /
 			(chart->y_ticks_count - 1);
 
-	double y_value;
 	for (i = 0; i < chart->y_ticks_count; i++) {
-		y_value = chart->y_limit_min +
+		value = chart->y_limit_min +
 			(double)i * y_tick_value_spacing;
-		snprintf(lbuffer, MAX_LABEL_SIZE, "%.1f", y_value);
-		width = get_text_width(chart->cr, chart->y_label_fontfamily,
+		rendered = dataplot_chart_prerender_value(chart, value,
+				chart->y_label_fontfamily,
 				CAIRO_FONT_SLANT_NORMAL,
 				CAIRO_FONT_WEIGHT_BOLD,
-				chart->y_label_fontsize, lbuffer);
-		height = get_text_height(chart->cr, chart->y_label_fontfamily,
+				chart->y_label_fontsize, NULL, NULL, 1);
+
+		chart->y_ticks_positions[i] = 
+			dataplot_chart_convert_to_y_scale(chart, value);
+		chart->y_ticks_labels[i] = rendered;
+	}
+}
+
+void
+dataplot_chart_render_x_axis_ticks(dataplot_chart_t *chart)
+{
+	double value;
+	unsigned int i;
+	char *rendered;
+
+	/* This is the size of the x-axis line */
+	double x_axis_width = chart->width - chart->margin_left -
+		chart->margin_right;
+
+	double x_limit_spread = chart->x_limit_max - chart->x_limit_min;
+
+	/* This is the number of labels we would like to have shown */
+	chart->x_ticks_count = x_axis_width / (chart->x_label_fontsize * 5.0);
+
+	chart->x_ticks_positions =
+		malloc(sizeof(double) * chart->x_ticks_count);
+	chart->x_ticks_labels = malloc(sizeof(char *) * chart->x_ticks_count);
+
+	double x_tick_value_spacing = x_limit_spread /
+			(chart->x_ticks_count - 1);
+
+	for (i = 0; i < chart->x_ticks_count; i++) {
+		value = chart->x_limit_min +
+			(double)i * x_tick_value_spacing;
+		rendered = dataplot_chart_prerender_value(chart, value,
+				chart->x_label_fontfamily,
 				CAIRO_FONT_SLANT_NORMAL,
 				CAIRO_FONT_WEIGHT_BOLD,
-				chart->y_label_fontsize, lbuffer);
+				chart->x_label_fontsize, NULL, NULL, 1);
+
+		chart->x_ticks_positions[i] = 
+			dataplot_chart_convert_to_x_scale(chart, value);
+		chart->x_ticks_labels[i] = rendered;
+	}
+}
+
+/**
+ * Determine the width/height of the x-axis labels.
+ * @private
+ */
+void
+dataplot_chart_calculate_x_axis_label_size(dataplot_chart_t *chart)
+{
+	unsigned int i;
+	unsigned int sample_max = 11;
+	double width, height, max_label_width = 0.0, max_label_height = 0.0;
+	double spacing;
+
+	spacing = (chart->x_limit_max - chart->x_limit_min) / (sample_max - 1);
+
+	double value;
+	for (i = 0; i < sample_max; i++) {
+		value = chart->x_limit_min + (double)i * spacing;
+		dataplot_chart_prerender_value(chart, value,
+				chart->x_label_fontfamily,
+				CAIRO_FONT_SLANT_NORMAL,
+				CAIRO_FONT_WEIGHT_BOLD,
+				chart->x_label_fontsize, &width, &height, 0);
 
 		if (width > max_label_width)
 			max_label_width = width;
 		if (height > max_label_height)
 			max_label_height = height;
+	}
 
-		chart->y_ticks_positions[i] = 
-			dataplot_chart_convert_to_y_scale(chart, y_value);
-		chart->y_ticks_labels[i] = strdup(lbuffer);
+	/* Define label max label sizes */
+	chart->x_label_max_width = max_label_width;
+	chart->x_label_max_height = max_label_height;
+}
+
+/**
+ * Determine the width/height of the x-axis labels.
+ * @private
+ */
+void
+dataplot_chart_calculate_y_axis_label_size(dataplot_chart_t *chart)
+{
+	unsigned int i;
+	unsigned int sample_max = 11;
+	double width, height, max_label_width = 0.0, max_label_height = 0.0;
+	double spacing;
+
+	spacing = (chart->y_limit_max - chart->y_limit_min) / (sample_max - 1);
+
+	double value;
+	for (i = 0; i < sample_max; i++) {
+		value = chart->y_limit_min + (double)i * spacing;
+		dataplot_chart_prerender_value(chart, value,
+				chart->y_label_fontfamily,
+				CAIRO_FONT_SLANT_NORMAL,
+				CAIRO_FONT_WEIGHT_BOLD,
+				chart->y_label_fontsize, &width, &height, 0);
+
+		if (width > max_label_width)
+			max_label_width = width;
+		if (height > max_label_height)
+			max_label_height = height;
 	}
 
 	/* Define label max label sizes */
@@ -289,8 +472,17 @@ dataplot_chart_render_y_axis_ticks(dataplot_chart_t *chart)
 void
 dataplot_chart_render_axes(dataplot_chart_t *chart)
 {
+	/* 
+	 * Calculate the max width and heights of the labels, it will be used
+	 * to get a proper size for the axes. The sizing is done on a sample
+	 * of 11.
+	 */
+	dataplot_chart_calculate_x_axis_label_size(chart);
+	dataplot_chart_calculate_y_axis_label_size(chart);
+
 	/* Generate the ticks (positions and labels) */
 	dataplot_chart_render_y_axis_ticks(chart);
+	dataplot_chart_render_x_axis_ticks(chart);
 
 	/* Select axes color */
 	cairo_set_source_rgb (chart->cr, 0.2, 0.2, 0.2);
@@ -298,16 +490,25 @@ dataplot_chart_render_axes(dataplot_chart_t *chart)
 
 	/* Draw axes */
 	cairo_new_path(chart->cr);
-	cairo_move_to(chart->cr, chart->margin_left + chart->y_label_max_width +
+	cairo_move_to(chart->cr, chart->margin_left +
+			chart->y_label_max_width +
 			chart->y_label_padding * 2.0, chart->margin_top);
-	cairo_line_to(chart->cr, chart->margin_left + chart->y_label_max_width +
-			chart->y_label_padding * 2.0, chart->height -
-			chart->margin_bottom);
+	cairo_line_to(chart->cr, chart->margin_left +
+			chart->y_label_max_width +
+			chart->y_label_padding * 2.0,
+			chart->height -
+			chart->margin_bottom -
+			chart->x_label_max_height -
+			chart->x_label_padding * 2.0);
 	cairo_line_to(chart->cr, chart->width - chart->margin_right,
-			chart->height - chart->margin_bottom);
+			chart->height -
+			chart->margin_bottom -
+			chart->x_label_max_height -
+			chart->x_label_padding * 2.0);
 	cairo_stroke(chart->cr);
 
 	dataplot_chart_render_y_axis_labels(chart);
+	dataplot_chart_render_x_axis_labels(chart);
 
 	cairo_set_source_rgb(chart->cr, 0, 0, 0);
 	cairo_fill(chart->cr);
@@ -322,8 +523,10 @@ void
 dataplot_chart_render(dataplot_chart_t *chart)
 {
 	FILE *fp;
+	/*
 	double data_x[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	double data_y[] = { 0.1, 0.2, 0.1, 0.1, 0.2, 0.3, 0.35, 0.4, 0.35, 0.3, 0.2 };
+	*/
 
 	chart->surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32,
 			chart->width, chart->height);
@@ -366,14 +569,12 @@ draw_y_label(cairo_t *cr, double y, const char *utf8)
 int
 main (int argc, char *argv[])
 {
-	cairo_text_extents_t extents;
-	cairo_pattern_t *pattern;
-
 	dataplot_chart_t *chart = dataplot_chart_new();
 	dataplot_chart_set_width(chart, 800);
 	dataplot_chart_set_height(chart, 200);
 	dataplot_chart_set_output_file(chart, "stuff.png");
 	dataplot_chart_set_y_limit(chart, 1, 50);
+	dataplot_chart_set_x_limit(chart, 200, 5000);
 
 	dataplot_chart_render(chart);
 
